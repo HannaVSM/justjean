6fc9f7b0661ee354a20b9634a6167ecb2111a1e0
HannaVSM <65380575+HannaVSM@users.noreply.github.com>
2026-07-14 21:25:45 -0500

    fix: final final definitivo

--- app/core/Constants.php
+++ app/core/Constants.php
@@ -15,4 +15,4 @@ define('APPROOT', dirname(dirname(__DIR__)));
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https" : "http";
 $host = $_SERVER['HTTP_HOST'];
 
-define('URLROOT', $protocol . '://' . $host . '/');
+define('URLROOT', getenv('APP_URL') ?: 'http://localhost/Sistema-inventario-justjean-main/');
